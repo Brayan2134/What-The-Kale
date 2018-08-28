56bc167ad909e9595c1d6ff809cd6705e23848f1
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-08-28 11:09:49 -0700

    Added Map On Footer - All Pages Except Product Page -

---
+++ Version 2/startbootstrap-grayscale-gh-pages/public_html/js/bgChangeOnload.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById('smoothieproduct');
+    if (x.style.backgroundColor === '#EEEEEE') {
+        x.style.backgroundColor = 'blue';
+    } else {
+        x.style.backgroundColor = 'white';
+    }
+}
